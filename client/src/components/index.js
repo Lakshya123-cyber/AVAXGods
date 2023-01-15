fb2c5913b1e6670791f7565c00c4949944d05109
Lakshya123-cyber <lakshyaraikwal123@gmail.com>
2023-01-15 19:58:57 +0800

    React Web3 Context and PageHOC

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
+import PageHOC from "./PageHOC";
+
+export { PageHOC, CustomInput, CustomButton };
---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from "./AVAXGods.json";
+
+export const ADDRESS = "0xD761370D8E63ff8aE4E57Da43d1C0a00b5A61180";
+export const { abi: ABI } = contract;
